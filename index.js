79cfc621ee72144e4e314b8786437de5b0359327
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-03-08 08:24:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-const express = require ("express");
-let app = express();
-app.use(logger);
-app.use(checkPermission)
+const express = require("express");
+const app = express()
+app.use(logger)
 
-app.get("/books",(req,res)=>{
-    console.log({ route: "/books"});
-    res.send({ route: "/books"});
+
+app.get("/books", (req, res)=>{
+    return res.send({route:"/books"})
 });
 
-app.get("/libraries",(req,res)=>{
-    console.log({ route: "/libraries", permission: true});
-    res.send({ route: "/libraries", permission: true});
-})
-
-app.get("/authors",(req,res)=>{
-console.log({ route: "/authors", permission: true})
-res.send({ route: "/authors", permission: true});
+app.get("/libraries",checkPermission("librarian"),(req, res)=>{
+    return res.send({ route: "/libraries",permission: req.role})
+   });
+   
+app.get("/authors",checkPermission("authors"),(req, res)=>{
+    return res.send({ route: "/authors", permission: req.role})
+    
 });
 
-function logger(req,res,next){
-    next();
+
+function checkPermission(user){
+    return function secondlogger(req, res, next){
+        if(user === "librarian" ){
+            res.permission = true
+            return next()
+        }
+        else if(user === "authors" ){
+            res.permission = true
+            return next()
+        }
+        else{
+            return res.send("user is on wrong path")
+        }
+    }
 }
-function checkPermission(req,res,next){
-if(req.path==true){
-    res.permission = req.route;
-}
-next();
-}
-app.listen(5000,()=>{
-    console.log("listen 5000");
-})
+
+function logger(req, res, next){
+    if(req.path === "/books"){
+        return next()
+    }  
+    else if(req.path === "/libraries"){
+        req.role = true;
+        return next()
+    }  
+    else if(req.path === "/authors"){
+        req.role = "Not allowed";
+       return next()
+    }
+    else{
+        return res.send("your path is worng")
+    }
+    
+};
+
+app.listen(5000, ()=>{
+    console.log("pop 5000 name")
+});
